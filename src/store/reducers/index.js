57163e5b51f60ed1025b7b9c49d5b594ea7a6fd1
ali <mehaboobali@capitalnumbers.com>
2021-03-15 12:01:14 +0530

    Add secretWord action creator

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineReducers } from 'redux';
-import successReducer from './successReducer';
-import guessWordReducer from './guessWordReducer';
-import secreteWordReducer from './secreteWordReducer';
+import { combineReducers } from "redux";
+import successReducer from "./successReducer";
+import guessWordReducer from "./guessWordReducer";
+import secreteWordReducer from "./secreteWordReducer";
 
 export default combineReducers({
   success: successReducer,
